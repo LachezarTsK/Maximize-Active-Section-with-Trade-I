6b371fdc35431f5084018f4801f7eda856e5d551
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-06-08 12:57:26 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,34 @@
+
+public class Solution {
+
+    public int maxActiveSectionsAfterTrade(String input) {
+        int totalNumberOfOnes = 0;
+        int numberOfZerosBeforeOnes = 0;
+        int maxNumberOfZerosToTransform = 0;
+
+        for (int i = 0; i < input.length(); ++i) {
+
+            int numberOfOnesBetweenZeros = 0;
+            while (i < input.length() && input.charAt(i) == '1') {
+                ++i;
+                ++totalNumberOfOnes;
+                ++numberOfOnesBetweenZeros;
+            }
+
+            int numberOfZerosAfterOnes = 0;
+            while (i < input.length() && input.charAt(i) == '0') {
+                ++i;
+                ++numberOfZerosAfterOnes;
+            }
+
+            if (numberOfZerosBeforeOnes > 0 && numberOfOnesBetweenZeros > 0 && numberOfZerosAfterOnes > 0) {
+                maxNumberOfZerosToTransform = Math.max(maxNumberOfZerosToTransform, numberOfZerosBeforeOnes + numberOfZerosAfterOnes);
+            }
+
+            --i;
+            numberOfZerosBeforeOnes = numberOfZerosAfterOnes;
+        }
+
+        return totalNumberOfOnes + maxNumberOfZerosToTransform;
+    }
+}
